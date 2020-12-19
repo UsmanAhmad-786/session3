046bf0698f5f788927848a1da9b8b6e4c5a94c90
Usman Ahmad <usman.ahmad6786@gmail.com>
2020-12-19 17:12:34 +0500

    initial commit

---
+++ src/lunch.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Lunch(props){
+
+return(
+<div>
+<h1>Welcome to {props.course}</h1>
+<hr/>
+<h1> we are all Intelligent</h1>
+<hr/>
+<h2>Bcz we are {props.nationality} and</h2>
+<hr/>
+<h3>we love <u>  {props.country}</u> </h3>
+<hr/>
+<h4>Thanku!</h4>
+</div>
+
+)
+
+}
+export default Lunch; 
